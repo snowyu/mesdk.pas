c633e07f7d9e59c2b36ae10d8595ad38c2d42d6b
riceball.lee@gmail.com <riceball.lee@gmail.com@e464e712-1140-0410-883d-b952c6e27fe8>
2008-11-21 07:48:50 +0000

    + [js]strToDate, dateToStr function and unit test

--- trunk/MeRemote/doc/request.js
+++ trunk/MeRemote/doc/request.js
@@ -1,4 +1,4 @@
-﻿/*
+﻿/**
 Script: Request.js
 @fileOverview Powerful all purpose Request Class. Uses XMLHTTPRequest and  Cross-Site Script.
 @Author:   original come from mootools 1.2
@@ -707,64 +707,3 @@ Request.JSON = new Class({
   }
 
 });
-
-
-/** helper function for inject */
-function Arguments(args) {
-  //convert arguments object to array
-  this.value = [].slice.call(args);
-}
-/**
-  @desc  inject the function
-  @param aOrgFunc     the original function to be injected.
-  @param aBeforeExec  this is called before the execution of the aOrgFunc.
-                      you must return the arguments(new Arguments(arguments)) if you wanna modify the arguments value of the aOrgFunc.
-                      it will stop the execution of the aOrgFunc if you return a value not an Arguments object nor a undefined value 
-  @param aAtferExec   this is called after the execution of the aOrgFunc.
-                      you must add a result argument at the last argument of the aAtferExec function if you wanna get the result value of the aOrgFunc.
-                      you must add a isDenied argument following the result argument if you wanna know whether the aOrgFunc is executed.
-                      you must return the result if you wanna modify the result value of the aOrgFunc .
-
-  @Usage  Obj.prototype.Method = Inject(Obj.prototype.Method, aFunctionBeforeExec[, aFunctionAtferExec]);
-  @version 1.1
-  @author  Aimingoo&Riceball
-  @history
-    V1.0 -- fiest released.
-    V1.1 -- 
-      Supports to denie the aOrgFunc execution in aBeforeExec.
-      Supports around in the aAtferExec, the aAtferExec be always executed even though denie the aOrgFunc execution in aBeforeExec.
-        + isDenied argument to the aAtferExec function. notice the aAtferExec whether the aOrgFunc is executed
-
-  eg:
-  var doTest = function (a) {return a};
-  function beforeTest(a) { alert('before exec: a='+a); a += 3; return new Arguments(arguments);};
-  function afterTest(a, result, isDenied) { alert('after exec: a='+a+'; result='+result+';isDenied='+isDenied); return result+5;};
-  
-  doTest = Inject(doTest, beforeTest, afterTest);
-  
-  alert (doTest(2));
-  the result should be 10.
-
-*/
-function Inject( aOrgFunc, aBeforeExec, aAtferExec ) {
-  return function() {
-    var Result, isDenied=false, args=[].slice.call(arguments);
-    if (typeof(aBeforeExec) == 'function') {
-      Result = aBeforeExec.apply(this, args);
-      if (Result instanceof Arguments) //(Result.constructor === Arguments)
-        args = Result.value;
-      else if (isDenied = Result !== undefined)
-        args.push(Result)
-    }
-
-    !isDenied && args.push(aOrgFunc.apply(this, args)); //if (!isDenied) args.push(aOrgFunc.apply(this, args));
-
-    if (typeof(aAtferExec) == 'function')
-      Result = aAtferExec.apply(this, args.concat(isDenied));
-    else 
-      Result = undefined;
-
-    return (Result !== undefined ? Result : args.pop());
-  }
-};
-
